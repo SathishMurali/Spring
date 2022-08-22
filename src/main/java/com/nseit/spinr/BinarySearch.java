ebbc80e2bcd21ce1fa7885d6cbcfdc8a4ac6c981
sathishmurali <greatsathish22@gmail.com>
2022-08-22 17:48:22 +0530

    06

--- src/main/java/com/nseit/spinr/BinarySearch.java
+++ src/main/java/com/nseit/spinr/BinarySearch.java
@@ -1,12 +1,14 @@
 package com.nseit.spinr;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BinarySearch {
 
     @Autowired
+    @Qualifier("Bubble")
     private SortAlg sortAlg;
     public int binarySearch(int[] numbers, int numberToSearch) {
         int[] sortedNumber = sortAlg.sort(numbers);
--- src/main/java/com/nseit/spinr/BubbleSort.java
+++ src/main/java/com/nseit/spinr/BubbleSort.java
@@ -3,7 +3,7 @@ package com.nseit.spinr;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Qualifier
+@Qualifier("Bubble")
 @Component
 public class BubbleSort implements SortAlg {
     public int[] sort(int[] numbers) {
